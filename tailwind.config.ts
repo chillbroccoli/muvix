f77b40885c165ce7d0895b55d750615b4fa9ba80
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-24 00:54:59 +0200

    add adrkMode by class

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,6 +3,7 @@ import tailwindTypography from '@tailwindcss/typography';
 import tailwindForms from '@tailwindcss/forms';
 
 export default <Partial<Config>>{
+  darkMode: 'class',
   theme: {
     extend: {},
   },
