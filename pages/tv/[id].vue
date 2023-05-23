983323eb35e9da081044956a288b8e427f7a9ca0
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-24 00:57:15 +0200

    add tv shows and single tv show pages

---
+++ pages/tv/[id].vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+import TvShowView from '~/components/views/TvShowView.vue';
+
+const route = useRoute();
+
+const { data, error } = await useFetch(`/api/tv/${route.params.id as string}`);
+
+if (error.value) {
+  throw createError(error.value);
+}
+</script>
+
+<template>
+  <div v-if="data">
+    <TvShowView :details="data.details" :similar="data.similar" />
+  </div>
+</template>
---
+++ pages/tv/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import TvShowsView from '~/components/views/TvShowsView.vue';
+
+const { data, error } = await useFetch('/api/tv');
+
+if (error.value) {
+  throw createError(error.value);
+}
+</script>
+
+<template>
+  <div v-if="data">
+    <TvShowsView
+      :top-rated="data.topRated"
+      :popular="data.popular"
+      :trending="data.trending"
+    />
+  </div>
+</template>
