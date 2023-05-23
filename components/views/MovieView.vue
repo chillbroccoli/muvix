aed86b5a5693e153347eead2420cfbd90a1aee23
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-24 00:55:52 +0200

    Create MovieView.vue

---
+++ components/views/MovieView.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { MovieResponse } from '~/utils/types';
+
+import ResourceFeatured from '../featured/ResourceFeatured.vue';
+
+defineProps<MovieResponse>();
+</script>
+
+<template>
+  <div class="w-full">
+    <ResourceFeatured :item="details" />
+    <MovieDetails :item="details" :similar="similar.results" />
+  </div>
+</template>
