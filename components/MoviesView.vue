3a86f99918e22a8a111c2c75a5b29659186a0c57
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:46:48 +0200

    Create MoviesView.vue

---
+++ components/MoviesView.vue
@@ -0,0 +1,38 @@
+<script lang="ts" setup>
+import { Movie, MoviesResponse } from '@/utils/types';
+import getRandomResource from '~/utils/helpers/getRandomResource';
+
+const props = defineProps<MoviesResponse>();
+
+const randomTrending = getRandomResource<Movie>([...props.topRated.results]);
+</script>
+
+<template>
+  <div class="h-full w-full">
+    <ClientOnly>
+      <ResourceFeatured :item="randomTrending" />
+    </ClientOnly>
+    <div className="pt-3">
+      <ViewToggle />
+    </div>
+
+    <ItemsList
+      :items="popular.results"
+      title="Popular"
+      shortened
+      href-type="movies"
+    />
+    <ItemsList
+      :items="topRated.results"
+      title="Top Rated"
+      shortened
+      href-type="movies"
+    />
+    <ItemsList
+      :items="nowPlaying.results"
+      title="Now Playing"
+      shortened
+      href-type="movies"
+    />
+  </div>
+</template>
