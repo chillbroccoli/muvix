8e98f66e6bc680a9060b004c552f751e39cefdbb
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:47:18 +0200

    add movies page

---
+++ pages/movies/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+const { data, error } = await useFetch('/api/movies');
+
+if (error.value) {
+  throw createError(error.value);
+}
+</script>
+
+<template>
+  <div v-if="data">
+    <MoviesView
+      :top-rated="data.topRated"
+      :popular="data.popular"
+      :now-playing="data.nowPlaying"
+    />
+  </div>
+</template>
