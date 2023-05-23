50f6c8f3e7653653ba887c04dc548131880fefd0
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:47:02 +0200

    add error page

---
+++ error.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <button type="button" @click="handleError">Clear errors</button>
+  </div>
+</template>
+<script lang="ts" setup>
+import { NuxtError } from 'nuxt/app';
+
+defineProps<{ error: NuxtError }>();
+
+const handleError = () => clearError({ redirect: '/' });
+</script>
