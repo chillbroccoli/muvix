d84714aec9f2256dd951cdc9019ce03871cacc77
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:46:55 +0200

    Create SideBarNav.vue

---
+++ components/SideBarNav.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+import nav from '@/utils/constants/nav';
+</script>
+
+<template>
+  <div class="h-full lg:mt-10">
+    <ul
+      class="flex flex-row items-center justify-center h-full space-x-4 lg:items-start lg:justify-start lg:space-x-0 lg:space-y-6 lg:flex-col"
+    >
+      <li
+        v-for="item in nav"
+        :key="item.href"
+        class="transition-colors duration-200 ease-in-out rounded-md hover:bg-pink-500/30"
+      >
+        <NuxtLink :to="item.href" class="flex w-full h-full gap-4 p-2">
+          <div>
+            <component :is="item.icon" :size="24" />
+          </div>
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+</template>
