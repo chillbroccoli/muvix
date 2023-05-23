56a3415b80f1c2135842d61952eedffdf061dd5b
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-24 00:56:27 +0200

    add single movie page

---
+++ pages/movies/[id].vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import MovieView from '~/components/views/MovieView.vue';
+
+const route = useRoute();
+
+const { data, error } = await useFetch(
+  `/api/movies/${route.params.id as string}`
+);
+
+if (error.value) {
+  throw createError(error.value);
+}
+</script>
+
+<template>
+  <div v-if="data">
+    <MovieView :details="data.details" :similar="data.similar" />
+  </div>
+</template>
