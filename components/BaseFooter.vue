a204a54850c21050b0086b40a7d65bc7302256c2
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:46:34 +0200

    Create BaseFooter.vue

---
+++ components/BaseFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="flex flex-col items-center justify-center lg:pb-12 font-light text-gray-500 font-sm pb-[100px]"
+  >
+    <p>&copy; {{ new Date().getFullYear() }}. All Rights Reserved.</p>
+    <p>
+      Data provided by
+      <a href="https://www.themoviedb.org/" class="underline"> TMDB </a>
+    </p>
+  </div>
+</template>
