39d67d28f7d8696602899ebe93ef39dac7e10a92
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-24 00:55:34 +0200

    Create ThemeSwitcher.vue

---
+++ components/theme-switcher/ThemeSwitcher.vue
@@ -0,0 +1,55 @@
+<script lang="ts" setup>
+import { Sun, Moon } from 'lucide-vue-next';
+import { Switch } from '@headlessui/vue';
+import useSettingsStore from '~/stores/settings';
+
+const store = useSettingsStore();
+
+useHead({
+  htmlAttrs: {
+    class: store.isDark ? 'dark' : 'light',
+  },
+});
+</script>
+
+<template>
+  <Switch
+    v-model="store.isDark"
+    :class="[
+      store.isDark ? 'bg-pink-600' : 'bg-gray-300',
+      'relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-pink-600 focus:ring-offset-2',
+    ]"
+    :default-checked="store.isDark"
+  >
+    <span class="sr-only">Use setting</span>
+    <span
+      :class="[
+        store.isDark ? 'translate-x-5' : 'translate-x-0',
+        'pointer-events-none relative inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out',
+      ]"
+    >
+      <span
+        :class="[
+          store.isDark
+            ? 'opacity-0 duration-100 ease-out'
+            : 'opacity-100 duration-200 ease-in',
+          'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
+        ]"
+        aria-hidden="true"
+      >
+        <Sun class="h-3 w-3 text-gray-400" />
+      </span>
+      <span
+        :class="[
+          store.isDark
+            ? 'opacity-100 duration-200 ease-in'
+            : 'opacity-0 duration-100 ease-out',
+          'absolute inset-0 flex h-full w-full items-center justify-center transition-opacity',
+        ]"
+        aria-hidden="true"
+      >
+        <Moon class="h-3 w-3 text-pink-600" />
+      </span>
+    </span>
+  </Switch>
+</template>
