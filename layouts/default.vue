a101a65c377f273453695154180582e96730de9c
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-24 00:54:32 +0200

    add html class based on theme

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,21 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import SideBar from '~/components/sidebar/SideBar.vue';
+import BaseFooter from '~/components/footer/BaseFooter.vue';
+import useSettingsStore from '~/stores/settings';
+
+const store = useSettingsStore();
+
+useServerHead({
+  htmlAttrs: {
+    class: store.isDark ? 'dark' : 'light',
+  },
+});
+</script>
 
 <template>
-  <div class="min-h-full bg-zinc-900 text-white">
+  <div
+    class="min-h-full bg-pink-50 dark:bg-zinc-900 dark:text-white text-gray-500"
+  >
     <SideBar />
     <div
       class="lg:w-[calc(100%_-_100px)] w-full lg:ml-[100px] h-full flex flex-col justify-between relative"
