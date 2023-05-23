1d987b33febae4138eb00b9b988290600972dae3
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:46:52 +0200

    Create SideBar.vue

---
+++ components/SideBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div
+    class="fixed z-10 lg:z-0 lg:top-0 bottom-0 left-0 right-0 lg:w-[100px] h-[70px] lg:h-full lg:border-r lg:border-pink-500/30 bg-zinc-800 lg:bg-transparent"
+  >
+    <div
+      class="flex flex-row items-center justify-center h-full lg:pt-6 lg:flex-col"
+    >
+      <div class="hidden lg:block">
+        <BaseLogo />
+      </div>
+      <SideBarNav />
+    </div>
+  </div>
+</template>
