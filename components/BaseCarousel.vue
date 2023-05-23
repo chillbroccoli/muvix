ed9a26f357d5aa36af7ea4f864ccaa377bc659fa
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:46:33 +0200

    Create BaseCarousel.vue

---
+++ components/BaseCarousel.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { Resource } from '../utils/types';
+
+type Props = {
+  items: Resource[];
+  hrefType?: 'movies' | 'tv';
+};
+
+withDefaults(defineProps<Props>(), {
+  items: () => [],
+  hrefType: 'movies',
+});
+</script>
+
+<template>
+  <div class="flex p-4 space-x-6 overflow-x-scroll flex-nowrap snap-x">
+    <CarouselItem
+      v-for="item in items"
+      :key="item.id"
+      :item="item"
+      :href-type="hrefType"
+    />
+  </div>
+</template>
