3173d64563168da6f4a83559612301e930b9badb
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-24 00:55:04 +0200

    Create BaseLoader.vue

---
+++ components/loader/BaseLoader.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { Loader } from 'lucide-vue-next';
+</script>
+
+<template>
+  <div class="flex items-center justify-center p-4 py-8">
+    <Loader class="w-12 h-12 text-pink-500 animate-spin" />
+  </div>
+</template>
