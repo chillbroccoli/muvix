345ca16d54cd0f7c6bffc18ff49cfdb9fc8b5597
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:46:36 +0200

    Create BaseLogo.vue

---
+++ components/BaseLogo.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import logo from '@/assets/images/movix-color.png';
+</script>
+
+<template>
+  <div class="relative w-16 h-16">
+    <NuxtLink to="/">
+      <img :src="logo" alt="Logo" fill />
+    </NuxtLink>
+  </div>
+</template>
