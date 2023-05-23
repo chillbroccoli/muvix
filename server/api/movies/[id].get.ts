612684be08debfd38f137e4020188a69d3feff5e
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-24 00:57:40 +0200

    add single movie api

---
+++ server/api/movies/[id].get.ts
@@ -0,0 +1,16 @@
+import { getDetails, getSimilar } from '../../../utils/api';
+import { MovieResponse } from '../../../utils/types';
+
+export default defineEventHandler(async (event) => {
+  const id = event.context.params?.id as string;
+
+  const [details, similar] = await Promise.all([
+    getDetails('movie', id),
+    getSimilar('movie', id),
+  ]);
+
+  return {
+    details,
+    similar,
+  } as MovieResponse;
+});
