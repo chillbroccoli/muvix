8b25d7595e6701145d98707185b00937ca26c35d
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:47:09 +0200

    add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="min-h-full bg-zinc-900 text-white">
+    <SideBar />
+    <div
+      class="lg:w-[calc(100%_-_100px)] w-full lg:ml-[100px] h-full flex flex-col justify-between relative"
+    >
+      <main class="w-full">
+        <slot />
+      </main>
+      <BaseFooter />
+    </div>
+  </div>
+</template>
