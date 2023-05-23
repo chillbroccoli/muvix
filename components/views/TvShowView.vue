00270e03992f0e5bffb80ebf0d383c18435a8b1a
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-24 00:56:00 +0200

    Create TvShowView.vue

---
+++ components/views/TvShowView.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+import { TvShowResponse } from '~/utils/types';
+
+import ResourceFeatured from '../featured/ResourceFeatured.vue';
+import TvShowDetails from '../tv-show/TvShowDetails.vue';
+
+defineProps<TvShowResponse>();
+</script>
+
+<template>
+  <div class="w-full">
+    <ResourceFeatured :item="details" />
+    <TvShowDetails :item="details" :similar="similar.results" />
+  </div>
+</template>
