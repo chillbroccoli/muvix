96787aad9cc5de5eaa80ff88f87e46b0717badad
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-23 12:47:13 +0200

    add home page

---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const { data, error } = await useFetch('/api');
+
+if (error.value) {
+  throw createError(error.value);
+}
+</script>
+
+<template>
+  <div v-if="data">
+    <HomeView :movies="data.movies" :tv="data.tv" />
+  </div>
+</template>
