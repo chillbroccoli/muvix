0f713a8bd081d86d7d313e7616634d75e52d50d5
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-25 02:35:03 +0200

    create SettingsBar component

---
+++ components/settings-bar/SettingsBar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import ViewToggle from '../view-toggle/ViewToggle.vue';
+import ThemeSwitcher from '../theme-switcher/ThemeSwitcher.vue';
+</script>
+
+<template>
+  <div className="pt-6 px-8 flex items-center justify-between">
+    <ViewToggle />
+    <ClientOnly>
+      <div class="block lg:hidden">
+        <ThemeSwitcher />
+      </div>
+    </ClientOnly>
+  </div>
+</template>
