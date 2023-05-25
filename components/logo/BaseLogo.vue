5e368f604a874a210ffba242f7021128e6421157
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-25 02:27:08 +0200

    display proper image depending on theme

--- components/logo/BaseLogo.vue
+++ components/logo/BaseLogo.vue
@@ -1,11 +1,19 @@
 <script lang="ts" setup>
-import logo from '~/assets/images/movix-color.png';
+import logoColor from '~/assets/images/movix-color.png';
+import logoBlack from '~/assets/images/movix-black.png';
+import useSettingsStore from '~/stores/settings';
+
+const store = useSettingsStore();
+
+const logo = computed(() => {
+  return store.isDark ? logoColor : logoBlack;
+});
 </script>
 
 <template>
   <div class="relative w-16 h-16">
     <NuxtLink to="/">
-      <img :src="logo" alt="Logo" fill />
+      <img :src="logo" alt="Logo" />
     </NuxtLink>
   </div>
 </template>
