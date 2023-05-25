8c2100ceaef59e9fbd883e782346a36e165623bb
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-25 02:30:06 +0200

    change bg for sidebar for light mode, update styles

--- components/sidebar/SideBar.vue
+++ components/sidebar/SideBar.vue
@@ -6,7 +6,7 @@ import SideBarNav from './SideBarNav.vue';
 
 <template>
   <div
-    class="fixed z-10 lg:z-0 lg:top-0 bottom-0 left-0 right-0 lg:w-[100px] h-[70px] lg:h-full lg:border-r lg:border-pink-500/30 bg-zinc-800 lg:bg-transparent"
+    class="fixed z-10 lg:z-0 lg:top-0 bottom-0 left-0 right-0 lg:w-[100px] h-[70px] lg:h-full lg:border-r lg:border-pink-500/30 bg-gray-200 dark:bg-zinc-800 lg:bg-transparent"
   >
     <div
       class="flex flex-row items-center justify-center h-full lg:pt-6 lg:flex-col"
@@ -15,7 +15,7 @@ import SideBarNav from './SideBarNav.vue';
         <BaseLogo />
       </div>
       <ClientOnly>
-        <div class="mt-6">
+        <div class="hidden mt-6 lg:block">
           <ThemeSwitcher />
         </div>
       </ClientOnly>
