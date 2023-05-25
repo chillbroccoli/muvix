4c4835e84583f1014601cfe247a582882a081a63
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-25 02:25:18 +0200

    update styles, code refactor

--- components/featured/ResourceFeatured.vue
+++ components/featured/ResourceFeatured.vue
@@ -15,30 +15,32 @@ const name = props.item?.title ?? props.item?.name ?? '';
 
 <template>
   <div class="w-full h-[450px] lg:h-[650px] relative">
-    <nuxt-img :src="imageSrc" :alt="name" class="h-full w-full" />
+    <nuxt-img :src="imageSrc" :alt="name" class="h-full w-full brightness-50" />
 
-    <div class="absolute top-0 bottom-0 left-0 right-0 bg-black/70">
+    <div class="absolute top-0 bottom-0 left-0 right-0">
       <div class="flex flex-col items-start justify-center h-full px-12">
-        <h2 class="text-4xl font-semibold tracking-tight">
+        <h2
+          class="text-4xl font-semibold tracking-tight text-pink-100 dark:text-pink-200"
+        >
           {{ name }}
         </h2>
-        <div class="flex items-end mt-4 space-x-2">
-          <Star class="text-pink-500" />
-          <span
-            v-if="item.vote_average"
-            class="text-sm font-light text-gray-100"
-          >
+        <div
+          v-if="item.vote_average && item.vote_average > 0"
+          class="flex items-end mt-4 space-x-2"
+        >
+          <Star class="fill-pink-500 stroke-pink-600" :size="24" />
+          <span class="text-lg lg:text-sm font-light text-gray-100">
             {{ item.vote_average.toFixed(1) }} rating
           </span>
 
-          <span class="text-sm font-light text-gray-100">
+          <span class="text-lg lg:text-sm font-light text-gray-100">
             {{ item?.release_date ?? item?.first_air_date }}
           </span>
         </div>
 
         <p
           v-if="item?.overview"
-          class="mt-4 max-w-[75%] lg:max-w-[50%] text-sm lg:text-md font-light text-gray-100 lg:tracking-tighter"
+          class="mt-4 max-w-full lg:max-w-[50%] font-light text-gray-100 lg:tracking-tighter"
         >
           {{ isSmallScreen ? trimLongText(item.overview, 250) : item.overview }}
         </p>
