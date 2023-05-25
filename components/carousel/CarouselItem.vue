d15ca3d220fa37b6fe3b7106a24fb6b8514adc65
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-25 02:24:14 +0200

    remove watchlist button

--- components/carousel/CarouselItem.vue
+++ components/carousel/CarouselItem.vue
@@ -1,8 +1,6 @@
 <script lang="ts" setup>
-import { Heart, HeartOff } from 'lucide-vue-next';
-import { POSTER_IMAGE } from '~/utils/constants/images';
-import useWatchlistStore from '~/stores/watchlist';
-import getWatchlistItemType from '~/utils/helpers/getWatchlistItemType';
+import { Star } from 'lucide-vue-next';
+import { FALLBACK_IMAGE, POSTER_IMAGE } from '~/utils/constants/images';
 import { Resource } from '~/utils/types';
 
 type Props = {
@@ -12,23 +10,10 @@ type Props = {
 
 const props = defineProps<Props>();
 
-const store = useWatchlistStore();
-
-const itemInWatchlist = computed(() => {
-  return store.watchlist.find(
-    (itemInList: Resource) => itemInList.id === props.item.id
-  );
-});
-
 const title = props.item?.title ?? props.item?.name ?? '';
-const watchlistItemType = getWatchlistItemType(props.item, props.hrefType);
 const imageSrc = props.item.poster_path
   ? `${POSTER_IMAGE.W342}${props.item.poster_path}`
-  : '/image_not_available.png';
-
-const handleAddToWatchlistClick = (item: Resource) => {
-  store.addToWatchlist(item, watchlistItemType);
-};
+  : FALLBACK_IMAGE;
 </script>
 
 <template>
@@ -39,23 +24,37 @@ const handleAddToWatchlistClick = (item: Resource) => {
     <div
       class="snap-start w-[225px] h-[350px] flex-shrink-0 shadow-md shadow-white/20 rounded-md overflow-hidden relative"
     >
-      <nuxt-img :src="imageSrc" :alt="title" class="w-full h-full" />
-      <button
-        type="button"
-        class="absolute rounded-bl-xl top-0 p-2 -right-[100%] bg-pink-500/80 group-hover:-right-0 transition-all duration-300 ease-in-out z-10"
-        @click.prevent="handleAddToWatchlistClick(item)"
-      >
-        <template v-if="itemInWatchlist">
-          <HeartOff class="text-white" :size="24" />
-        </template>
-        <template v-else>
-          <Heart class="text-white" :size="24" />
-        </template>
-      </button>
+      <nuxt-img
+        :src="imageSrc"
+        :alt="title"
+        class="w-full h-full transition-all duration-200 ease-in-out group-hover:scale-110 brightness-75"
+      />
     </div>
 
-    <div
-      class="absolute top-0 bottom-0 left-0 right-0 transition-all duration-200 ease-in-out bg-black/20 group-hover:bg-black/50"
-    ></div>
+    <div>
+      <div class="flex flex-col py-2 space-y-1">
+        <p class="text-sm font-semibold tracking-tighter">{{ title }}</p>
+
+        <div v-if="item.vote_average" class="flex items-center">
+          <template v-for="rating in [1, 2, 3, 4, 5]" :key="rating">
+            <Star
+              :class="[
+                Math.floor(item.vote_average / 2) >= rating
+                  ? 'fill-pink-400/50 dark:fill-pink-500'
+                  : 'fill-pink-50 dark:fill-pink-200',
+                'stroke-pink-600',
+              ]"
+              :size="18"
+            />
+          </template>
+          <span
+            v-if="item.vote_average && item.vote_average > 0"
+            class="ml-1 text-sm font-light tracking-tighter"
+          >
+            {{ item.vote_average.toFixed(2) }}
+          </span>
+        </div>
+      </div>
+    </div>
   </NuxtLink>
 </template>
