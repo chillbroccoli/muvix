5474529d2fc000b10af7a56b75cef06107b408f4
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-28 16:20:19 +0200

    adjust pagination buttons styles for light mode

--- components/pagination/BasePagination.vue
+++ components/pagination/BasePagination.vue
@@ -20,7 +20,7 @@ const emit = defineEmits<Emits>();
     <div className="flex items-center gap-2">
       <button
         type="button"
-        className="cursor-pointer rounded-md border border-pink-600/60 bg-pink-600/50 py-1.5  px-3 text-gray-200 hover:bg-pink-600/20 disabled:cursor-not-allowed disabled:bg-pink-600/5"
+        className="cursor-pointer rounded-md border border-pink-600/60 bg-pink-600 dark:bg-pink-600/50 py-1.5  px-3 text-gray-200 hover:bg-pink-600/80 dark:hover:bg-pink-600/20 disabled:cursor-not-allowed disabled:bg-pink-800 dark:disabled:bg-pink-600/20"
         :disabled="currentPage === 1"
         @click="emit('goToPreviousPage')"
       >
@@ -28,7 +28,7 @@ const emit = defineEmits<Emits>();
       </button>
       <button
         type="button"
-        className="cursor-pointer rounded-md border border-pink-600/60 bg-pink-600/50 py-1.5 px-3 text-gray-200 hover:bg-pink-600/20 disabled:cursor-not-allowed disabled:bg-pink-600/5"
+        className="cursor-pointer rounded-md border border-pink-600/60 bg-pink-600 dark:bg-pink-600/50 py-1.5 px-3 text-gray-200 hover:bg-pink-600/80 dark:hover:bg-pink-600/20 disabled:cursor-not-allowed disabled:bg-pink-800 dark:disabled:bg-pink-600/20"
         :disabled="currentPage === totalPages"
         @click="emit('goToNextPage')"
       >
