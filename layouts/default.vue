e95342292f00e3cdf616ad21380c420d91154ace
Kacper Malek <kacper.malek0828@gmail.com>
2023-05-28 14:38:45 +0200

    add css for page transitions

--- layouts/default.vue
+++ layouts/default.vue
@@ -40,3 +40,15 @@ useServerHead({
     </div>
   </div>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.15s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
